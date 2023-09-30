eabb95a290534826b48fee78712fc2bd70938eb9
kohei.mizuki <kohei.mizuki@linecorp.com>
2023-09-30 15:24:47 +0900

    fix

--- localhost/sql/init.sh
+++ localhost/sql/init.sh
@@ -47,7 +47,7 @@ SECURE_DIR=${DIR:-/var/lib/mysql-files/}
 
 # Load data
 
-sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
+#sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
 
 echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLACE INTO TABLE user_presents FIELDS ESCAPED BY '|' IGNORE 1 LINES ;" | mysql -u"$ISUCON_DB_USER" \
         -p"$ISUCON_DB_PASSWORD" \
