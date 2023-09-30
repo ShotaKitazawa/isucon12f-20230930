66bfe95afbc496083e1ccc753171ca238996d786
kohei.mizuki <kohei.mizuki@linecorp.com>
2023-09-30 15:00:47 +0900

    add index

--- localhost/sql/init.sh
+++ localhost/sql/init.sh
@@ -38,6 +38,13 @@ mysql -u"$ISUCON_DB_USER" \
 		"$ISUCON_DB_NAME" -e \
     "alter table user_present_all_received_history add index user_id_present_all_id(user_id, present_all_id);"
 
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" -e \
+    "alter table user_presents add index user_id_deleted_at_created_at_desc_index(user_id, deleted_at, created_at desc);"
+
 # Load data
 
 sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
